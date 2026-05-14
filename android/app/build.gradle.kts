bf643373bc77552c0383ee906829b75701174d08
jydv402 <jydv402@gmail.com>
2026-05-15 00:11:49 +0530

    Fix workflow

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -58,10 +58,10 @@ android {
 
     signingConfigs {
         create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String?
-            keyPassword = keystoreProperties["keyPassword"] as String?
-            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
-            storePassword = keystoreProperties["storePassword"] as String?
+            keyAlias = System.getenv("KEY_ALIAS") ?: keystoreProperties["keyAlias"] as String?
+            keyPassword = System.getenv("KEY_PASSWORD") ?: keystoreProperties["keyPassword"] as String?
+            storeFile = (System.getenv("KEYSTORE_FILE") ?: keystoreProperties["storeFile"] as String?)?.let { file(it) }
+            storePassword = System.getenv("KEYSTORE_PASSWORD") ?: keystoreProperties["storePassword"] as String?
         }
     }
 
